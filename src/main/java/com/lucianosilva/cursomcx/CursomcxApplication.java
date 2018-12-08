15826af855b260ccc0921e79c16247c8e12cac89
Luciano Drop <lucianojavax@gmail.com>
2018-12-07 22:41:47 -0300

    Primeiro Projeto

---
+++ src/main/java/com/lucianosilva/cursomcx/CursomcxApplication.java
@@ -0,0 +1,12 @@
+package com.lucianosilva.cursomcx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcxApplication.class, args);
+	}
+}
